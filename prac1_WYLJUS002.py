952b4dc38201b123f1bde3dfefc1094400a21ae3
SpectrumX <wylieza@gmail.com>
2019-07-23 14:22:43 +0200

    Blink an led code implemeted

--- prac1_WYLJUS002.py
+++ prac1_WYLJUS002.py
@@ -8,10 +8,32 @@ Date: <21/07/2019>
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
+import time
+
+#SETUP
+
+#pins
+GPIO.setmode(GPIO.BOARD)
+
+led0 = 11
+led1 = 13
+led2 = 15
+
+GPIO.setup(led0, GPIO.OUT)
+GPIO.setup(led1, GPIO.OUT)
+GPIO.setup(led2, GPIO.OUT)
+
+#interrupts
+#GPIO.add_event_detect(BTN_B, GPIO.RISING, method_on_interrupt)
+#GPIO.add_event_detect(BTN_PIN, GPIO.FALLING, callback=callback_method(),bouncetime=300)
 
 # Logic that you write
 def main():
     print("write your logic here my dude")
+    GPIO.output(led0, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(led0,GPIO.LOW)
+    time.sleep(1)
 
 
 # Only run the functions if 
