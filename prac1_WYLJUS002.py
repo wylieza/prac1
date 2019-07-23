a12d586abc9fd30badf70939a89ba07d0b5c3801
SpectrumX <wylieza@gmail.com>
2019-07-23 15:30:08 +0200

    Bin counter working

--- prac1_WYLJUS002.py
+++ prac1_WYLJUS002.py
@@ -12,12 +12,15 @@ import time
 
 #SETUP
 
+#globals
+count = 0
+
 #pins
 GPIO.setmode(GPIO.BOARD)
 
-led0 = 11
+led2 = 11
 led1 = 13
-led2 = 15
+led0 = 15
 btn0 = 16
 btn1 = 18
 
@@ -28,28 +31,50 @@ GPIO.setup(btn0, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(btn1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 #methods
-def callback_method_btn0(channel):
-    GPIO.output(led0, GPIO.HIGH)
-    print("hello" + str(channel))
+def callback_method_btn0(channel): #incremet
+    global count
+    if(count >= 7):
+        count = 0
+    else:
+        count += 1
 
-def callback_method_btn1(channel):
-    GPIO.output(led0, GPIO.LOW)
-    print("there")
+
+def callback_method_btn1(channel): #decrement
+    global count
+    if(count <= 0):
+        count = 7
+    else:
+        count -= 1
 
 
 #interrupts
 #GPIO.add_event_detect(BTN_B, GPIO.RISING, method_on_interrupt)
-GPIO.add_event_detect(btn0, GPIO.FALLING, callback=callback_method_btn0,bouncetime=300)
-GPIO.add_event_detect(btn1, GPIO.FALLING, callback=callback_method_btn1,bouncetime=300)
+GPIO.add_event_detect(btn0, GPIO.FALLING, callback=callback_method_btn0,bouncetime=250)
+GPIO.add_event_detect(btn1, GPIO.FALLING, callback=callback_method_btn1,bouncetime=250)
 
 # Logic that you write
 def main():
-    print("write your logic here my dude")
-    #if GPIO.input(btn0) == GPIO.HIGH:
-     #   GPIO.output(led0, GPIO.HIGH)
+    print("Display Update")
+    temp = count
 
-    #GPIO.output(led0,GPIO.LOW)
-    time.sleep(1)
+    if temp >= 4:
+        GPIO.output(led2, GPIO.HIGH)
+        temp -= 4
+    else:
+        GPIO.output(led2, GPIO.LOW)
+
+    if temp >= 2:
+        GPIO.output(led1, GPIO.HIGH)
+        temp -= 2
+    else:
+        GPIO.output(led1, GPIO.LOW)
+
+    if temp >= 1:
+        GPIO.output(led0, GPIO.HIGH)
+        temp -= 1
+    else:
+        GPIO.output(led0, GPIO.LOW)
+
 
 
 
